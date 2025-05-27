d0dbe85a01791e4976ac6cd886a2f4acb0e6e10c
Lev Akhnazarov <lakhnazarov@icloud.com>
2025-05-27 18:52:55 +0100

    add dotenv configuration and BEPolia network to Hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config({ path: ".env" });
 
 module.exports = {
   solidity: "0.8.22",
@@ -8,7 +9,11 @@ module.exports = {
     localhost: {
       url: "http://localhost:7545",
       chainId: 1337
+    },
+    bepolia: {
+      url: "https://bepolia.rpc.berachain.com",
+      chainId: 80069,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     }
-    // add testnet/mainnet config here if needed
   }
 };
