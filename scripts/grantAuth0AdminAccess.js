bc67c6857ae8cf4ecf63b983391449bb7861e1ca
kirsty <kirstygoodary1@gmail.com>
2025-08-11 11:46:55 +0100

    clean up: include all code

--- scripts/grantAuth0AdminAccess.js
+++ scripts/grantAuth0AdminAccess.js
@@ -7,18 +7,18 @@ async function main() {
 
     const [owner] = await ethers.getSigners();
 
-    // const projectId = 1;
-    // const to = "0x61b33e2b591202d68A3ac4950F1687A0f67E2d80"; // auth0 wallet address
-    // const membershipType = "write:admin" // grant minting and revoking rights
-    // const expiration = 0;
-    // const transferable = true; 
+    const projectId = 1;
+    const to = "0x61b33e2b591202d68A3ac4950F1687A0f67E2d80"; // auth0 wallet address
+    const membershipType = "write:admin" // grant minting and revoking rights
+    const expiration = 0;
+    const transferable = true; 
 
-    // const tx = await RevocableNFT.connect(owner).mint(projectId, to, membershipType, expiration, transferable);
-    // const receipt = await tx.wait(1);
+    const tx = await RevocableNFT.connect(owner).mint(projectId, to, membershipType, expiration, transferable);
+    const receipt = await tx.wait(1);
 
-    // console.log(`Minting transaction successful: ${receipt.status === 1}`);
+    console.log(`Minting transaction successful: ${receipt.status === 1}`);
 
-    // const tokenId = Number(receipt.logs[0].args[1]);
+    const tokenId = Number(receipt.logs[0].args[1]);
 
     const membership = await RevocableNFT.viewMembership(28);
     console.log(`Membership minted: membership ${membership.membershipType} for project ${membership.projectId} to ${membership.user} with token ID ${membership.tokenId}`);
